37333fb12b26ce09959c2fa0d8fe37b857c7f7fb
Neisanael <nathan123sembung@gmail.com>
2023-05-08 21:28:36 +0700

    Update coba.py

--- coba.py
+++ coba.py
@@ -21,8 +21,27 @@ class hewan :
 
     def displayOutput(self):
         print(self.jenisHewan," && ", self.namaHewan," && ", self.suaraHewan)
-    
-hewan1 = hewan("Anjing", "Blacky", "Whoof")
-hewan1.displayOutput()
-hewan1.setJenisHewan("Kucing")
-hewan1.displayOutput()
+
+listHewan = []
+while True:
+    choice = input("Masukkan Pilihan:\n[0] Keluar\n[1] Masukkan Data\n[2] Selesai Input\n")
+
+    if choice == "0":
+        print("Keluar")
+        break
+    elif choice == "1":
+        print("Masukkan Data")
+        namaHewan = input(str("Sebutkan nama hewan? : "))
+        jenisHewan = input(str("Jenis Hewanya apa? : "))
+        suaraHewan = input(str("Suaranya seperti apa? : "))
+        hwn = hewan(namaHewan, jenisHewan, suaraHewan)
+        listHewan.append(hwn)
+
+    elif choice == "2":
+        print("Selesai Input")
+        for animal in listHewan:
+            animal.displayOutput()
+        continue
+    else:
+        print("Pilihan Tidak Valid. Silahkan Ulangi.")
+
